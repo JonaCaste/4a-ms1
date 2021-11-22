32b4c200cc5aa27f44531a062b85ae862ed2690a
Javier Cardenas <jreyescardenas42>
2021-11-21 21:06:09 -0500

    creacion nueva aplicacion profesional

--- profesional/views.py
+++ profesional/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from rest_framework                 import generics 
-from .serializer                    import PersonaProfesionalSerializer
-from .models                        import PersonaProfesional
+from rest_framework   import generics 
+from .serializer      import PersonaProfesionalSerializer
+from .models          import PersonaProfesional
+from django.db.models.query import QuerySet
 
 class PersonaProfesionalListCreate(generics.ListCreateAPIView):
     queryset = PersonaProfesional.objects.all()
